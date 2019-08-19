906a80b658ca674c573453d7e2ac036c8ca52869
Артур Чистяк (Ksu) <msksushantonova@gmail.com>
2019-08-19 19:09:06 +0300

    run server.spec.test

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,10 @@
 import Koa from 'koa';
+import { Server } from './server';
 
-export const app = new Koa();
+const app = new Koa();
 
-export const port: number = Number(process.env.PORT);
+const server = new Server(app);
+server.start();
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
-});
 
-app.listen(port, () => {
-  console.log('server running on port ', port);
-});
+
